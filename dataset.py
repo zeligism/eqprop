1cfedfa83ed702712fa240013a1d53d4fe9b71d1
Abzollo <ma7aseem@gmail.com>
2018-10-09 14:50:38 -0700

    MNIST dataset loader

---
+++ dataset.py
@@ -0,0 +1,37 @@
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+def dataset(batch_size):
+    ### Prepare training and test sets ###
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    trainset = torchvision.datasets.MNIST(
+        root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.MNIST(
+        root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    return trainloader, testloader
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+
+
+
+
+
+
