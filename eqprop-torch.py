08d566c279c7368db2c145c1c92978986d3dce07
Abzollo <ma7aseem@gmail.com>
2018-10-10 14:35:08 -0700

    Flipped the sign in the weight update equation. The cost seems to be minimized now, but training on more than one layer is very slow on a cpu. I also refactor some of the code, adding a training procedure, etc.

--- eqprop-torch.py
+++ eqprop-torch.py
@@ -1,8 +1,8 @@
 
+import time
 import torch
 
 from training_configs import *
-
 from dataset import dataset
 
 
@@ -22,10 +22,10 @@ class Net:
             torch.randn([BATCH_SIZE, l1, l2])
             for l1, l2 in zip(layers_sizes[:-1], layers_sizes[1:])]
         self.biases = [
-            torch.randn([BATCH_SIZE, l])
+            torch.zeros([BATCH_SIZE, l])
             for l in layers_sizes[:-1]]
         self.states = [
-            torch.randn([BATCH_SIZE, l], requires_grad=True)
+            torch.zeros([BATCH_SIZE, l], requires_grad=True)
             for l in layers_sizes]
 
     
@@ -48,29 +48,17 @@ class Net:
             # Sum of bias_i * rho(s_i)
             energy -= torch.sum(self.biases[i] * rho(states[i]), dim=-1)
 
-        return energy.sum()
+        return energy.mean()
 
 
-    def cost(self, output_layer_state, y):
+    def cost(self, y):
         """
         Calculates the cost between the state of the last layer of the network
         with the output y. The cost is just the distance (L2 loss).
         """
-        cost = torch.sum((output_layer_state - y) ** 2, dim=-1)
+        cost = torch.sum((self.output_state() - y) ** 2, dim=-1)
         
-        return cost.sum()
-
-
-    def total_energy(self, states, y=None):
-        """
-        Calculates the total energy (plus the cost in the weakly clamped phase).
-        XXX Not used
-        """
-        energy = self.energy(states)
-        if y is not None:
-            energy += self.beta * self.cost(states, y)
-
-        return energy
+        return cost.mean()
 
 
     def clamp_input(self, x):
@@ -107,7 +95,7 @@ class Net:
         # Calculate the total energy with the cost if y is given
         energy = self.energy(self.states)
         if y is not None:
-            energy += self.beta * self.cost(self.output_state(), y)
+            energy += self.beta * self.cost(y)
 
         # Calculate the gradients
         energy.backward()
@@ -117,10 +105,38 @@ class Net:
             # Notice the negative sign because ds/dt = -dE/ds (partial d)
             self.states[i] = self.states[i] - dt * self.states[i].grad
             # We re-attach the states after updating them to reset gradient path
-            self.states[i] = self.states[i].detach().requires_grad_()
+            self.states[i] = self.states[i].clamp(0,1).detach().requires_grad_()
 
         return energy
 
+
+    def eqprop(self, x, y, validation=False):
+        """
+        Trains the network on one example (x,y) using equilibrium propagation.
+        """
+
+        # First clamp the input
+        self.clamp_input(x)
+
+        # Run free phase
+        for i in range(N_ITER_1):
+            energy = self.step()
+
+        if validation:
+            return energy.item(), self.cost(y).item()
+
+        # Collect states
+        free_states = [s.data for s in self.states]
+
+        # Run weakly clamped phase
+        for i in range(N_ITER_2):
+            energy = self.step(y)
+
+        #print("Updating weights...")
+        self.update_weights(free_states)
+
+        return energy.item(), self.cost(y).item()
+    
 
     def update_weights(self, free_states, dt=DELTA):
         """
@@ -137,44 +153,20 @@ class Net:
         energy = 1 / self.beta * (clamped_energy - free_energy)
         energy.backward()
 
-        # Update weights and biases
+        # Update weights and biases (note the negative sign)
         for i in range(len(self.W)):
-            self.W[i] = self.W[i] + dt * lr[i] * self.W[i].grad
+            self.W[i] = self.W[i] - lr[i] * self.W[i].grad
             self.W[i] = self.W[i].detach().requires_grad_()
 
         for i in range(len(self.biases)):
-            self.biases[i] = self.biases[i] + dt * lr[i] * self.biases[i].grad
+            self.biases[i] = self.biases[i] - lr[i] * self.biases[i].grad
             self.biases[i] = self.biases[i].detach().requires_grad_()
 
         [w.detach_() for w in self.W]
         [b.detach_() for b in self.biases]
 
 
-    def eqprop(self, x, y, validation=False):
-        """
-        Trains the network on one example (x,y) using equilibrium propagation.
-        """
-
-        # First clamp the input
-        self.clamp_input(x)
-
-        # Run free phase until states settle
-        #print("First phase...")
-        for i in range(N_ITER_1):
-            energy = self.step()
-
-        # Collect states
-        free_states = [s.data for s in self.states]
-
-        # Test XXX
-        #print("Second phase...")
-        for i in range(N_ITER_2):
-            energy = self.step(y)
-
-        #print("Updating weights...")
-        if not validation: self.update_weights(free_states)
-
-        return self.cost(self.output_state(), y).item()
+##############################################
 
 
 def index_to_onehot(index, num_indices):
@@ -185,19 +177,33 @@ def index_to_onehot(index, num_indices):
 
 
 def train(net, trainloader):
-    for i, data in enumerate(trainloader):
-        x, y = data
+    start_time = time.time()
+    for epoch in range(EPOCHS):
+        running_energy = running_cost = 0.0
+        for i, data in enumerate(trainloader):
+            x, y = data
 
-        # Prepare data
-        x = x.view(-1, LAYER_SIZES[0])
-        y_onehot = index_to_onehot(y, LAYER_SIZES[-1])
+            # Prepare data
+            x = x.view(-1, LAYER_SIZES[0])
+            y_onehot = index_to_onehot(y, LAYER_SIZES[-1])
 
-        # Train on (x,y) using equilibrium propagation
-        cost = net.eqprop(x, y_onehot)
-        print(i, "cost =", cost)
+            # Train on (x,y) using equilibrium propagation
+            energy, cost = net.eqprop(x, y_onehot)
 
+            running_energy += energy
+            running_cost += cost
+            if (i + 1) % CHECKPOINT == 0:
+                avg_cost = running_cost / CHECKPOINT
+                print("[%d, %d] energy = %.3f, cost = %.3f" % (epoch+1, i+1,
+                    running_energy / CHECKPOINT, running_cost / CHECKPOINT))
+                print("Time elapsed = %ds" % (time.time() - start_time))
+                running_energy = running_cost = 0.0
+
+    print("\nEPOCH: [%d/%d] energy = %.3f, cost = %.3f\n" % (epoch+1, EPOCHS, energy, cost))
+            
 
 def test(net, testloader):
+    running_hits = iterations = 0
     for i, data in enumerate(testloader):
         x, y = data
 
@@ -206,13 +212,19 @@ def test(net, testloader):
         y_onehot = index_to_onehot(y, LAYER_SIZES[-1])
 
         # Train on (x,y) using equilibrium propagation
-        cost = net.eqprop(x, y_onehot, validation=True)
-        print(i, "cost =", cost)
+        energy, cost = net.eqprop(x, y_onehot, validation=True)
+        print("[%d] energy = %.3f, cost = %.3f" % (i, energy, cost))
 
         # Calculate hits
         _, index = net.output_state().max(dim=-1)
         hits = (y == index).sum().item()
         print("hits =", hits, "out of", BATCH_SIZE)
+
+        running_hits += hits
+        iterations += 1
+
+    print("\nAverage hits =", running_hits / iterations)
+    print("Error =", 100 * (1 - running_hits / (iterations * BATCH_SIZE)))
 
 
 def main():
